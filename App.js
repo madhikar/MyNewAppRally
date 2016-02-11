7fbf2ed0649b86bfeade990c6e894a27c4a676a6
madhikar <adhikarimanoj1@gmail.com>
2016-02-11 22:03:16 +0000

    addGrid

--- App.js
+++ App.js
@@ -5,7 +5,23 @@ Ext.define('CustomApp', {
     launch: function() {
         //Write app code here
         console.log('this is version2 !');
+        this.add ({
+            xtype: 'rallygrid',
+            columnCfgs: [ 'FormattedID',
+                          'Name',
+                          'State',
+                          'SubmittedBy'
+                          ],
+                     storeConfig: {
+                     model: 'defect'
+                 }
+        })
 
         //API Docs: https://help.rallydev.com/apps/2.0/doc/
     }
+    
+    _loadGrid : function() {
+        
+    }
+    
 });
